2652cf44006a1eb88e9b6dc5ff9f6451889a8e3e
Aamna Rizwan <aamnarizwan7@gmail.com>
2025-08-29 11:54:55 +0500

    Update q2.cpp

--- Lab 2/q2.cpp
+++ Lab 2/q2.cpp
@@ -1,1 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int bench, seat;
+cout << "Enter benches\n";
+cin >> bench;
+cout << "Enter seats\n";
+cin >> seat;
+
+int **arr = new int *[bench];
+for(int i=0;i< bench;i++){
+    arr[i] = new int[seat];
+}
+
+cout << "enter 0 for unoccupied and 1 for occupied\n";
+for(int i=0;i<bench;i++){
+    for(int j=0;j<seat;j++){
+        cout << "Enter for bench " << i+1 <<" and seat  " << j+1 << endl;
+        cin >> arr[i][j];
+    }
+}
+
+cout << "final form :\n";
+for(int i=0;i<bench;i++){
+    for(int j=0;j<seat;j++){ 
+        cout << arr[i][j] << " ";
+    }
+    cout << endl;
+}
+
+for(int i=0;i<bench;i++){
+    delete[] arr[i];
+}
+delete[] arr;
+
+}
 
