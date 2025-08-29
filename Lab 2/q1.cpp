f4b48390ab3853fca158c6df3ab7853e4e6f0bdc
Aamna Rizwan <aamnarizwan7@gmail.com>
2025-08-29 11:39:10 +0500

    Update q1.cpp

--- Lab 2/q1.cpp
+++ Lab 2/q1.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+using namespace std;
 
+int main(){
+    int size;
+    cout << "enter the size\n";
+    cin >> size;
+    int *arr = new int[size]{}; //curly brackets sey zero intialize hogya
+    for(int i=0;i<size;i++){
+        cout << "Enter the value at position " << i+1 << endl;
+        cin >> arr[i];
+    }
+    cout << "You entered:\n";
+    for(int i=0;i<size;i++){
+        cout << arr[i] << ", " ;
+    }
+
+    delete[] arr;
+
+}
