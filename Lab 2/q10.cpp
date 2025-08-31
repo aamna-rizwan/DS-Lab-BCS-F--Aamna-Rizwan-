c3736ec6a2f54676bb4b8e7254b2f2be2768c54f
Aamna Rizwan <aamnarizwan7@gmail.com>
2025-08-31 14:02:36 +0500

    Update q10.cpp

--- Lab 2/q10.cpp
+++ Lab 2/q10.cpp
@@ -1,1 +1,50 @@
+#include <iostream>
+using namespace std;
 
+int main(){
+    bool available=false;
+    int row;
+    cout << "Enter number of rows (catefories)\n";
+    cin >> row;
+    int **id = new int*[row];
+    int *books = new int[row];
+    for(int i=0;i<row;i++){
+        cout << "Enter number of books for Category : " << i+1 << endl;
+        cin >> books[i];
+    }
+    for(int i=0;i<row;i++){
+        id[i]=new int[books[i]];
+    }
+    for(int i=0;i<row;i++){
+        cout << "Category : " << i+1 << endl;
+        for(int j=0;j<books[i];j++){
+            cout << "Enter book ID for book " << j+1 << endl;
+            cin >> id[i][j];
+        }
+    }
+    for(int i=0;i<row;i++){
+        for(int j=0;j<books[i];j++){
+            cout << id[i][j] << " ";
+        }cout << endl;
+    }
+     int search;
+    cout << "enter id to search\n";
+    cin >> search;
+    for(int i=0;i<row;i++){
+        for(int j=0;j<books[i];j++){
+            if(search == id[i][j]){
+                available = true;
+                cout << "Book Available and belongs to Category " << i+1 << endl;
+            }
+        }
+    }
+    if(available==false){
+        cout << "book not found!\n";
+    }
+
+    for(int i=0;i<row;i++){
+        delete[] id[i];
+    }delete[] id;
+    delete[] books;
+
+}
