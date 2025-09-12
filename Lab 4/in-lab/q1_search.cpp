53575964b1986d3ab864c2ad25dc4f8cdd47f4b5
Aamna Rizwan <aamnarizwan7@gmail.com>
2025-09-12 11:39:51 +0500

    Update q1_search.cpp

--- Lab 4/in-lab/q1_search.cpp
+++ Lab 4/in-lab/q1_search.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+  int n;
+  int num;
+  bool found = false;
+  cout << "Enter number of elements\n";
+  cin >> n;
+  int *arr = new int[n]{};
+  for(int i =0;i<n;i++){
+      cout << "Enter element " << i+1 << endl;
+      cin >> arr[i];
+  }
+  cout << "Enter the value you want to search for\n";
+  cin >> num;
+   for(int i =0;i<n;i++){
+      if (found==false){
+          if(arr[i]==num){
+              cout << "Index of value :  " << i << endl;
+              found=true;
+              return 0;
+          }
+      }
+  }cout << "not found!\n";
+  delete []arr;
+}
