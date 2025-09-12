ff57b6f1a3e69ac725280ab5fbc476bb98c552fc
Aamna Rizwan <aamnarizwan7@gmail.com>
2025-09-12 12:21:45 +0500

    Update q1_sort.cpp

--- Lab 4/in-lab/q1_sort.cpp
+++ Lab 4/in-lab/q1_sort.cpp
@@ -16,8 +16,8 @@ int main() {
       cout << arr[i] << " " ;
   }
   for(int i=0;i<n;i++){
-      for(int j=0;j<n-1;j++){
-          if(arr[j]>arr[j+1]){
+      for(int j=0;j<n-i-1;j++){
+          if(arr[j]<arr[j+1]){
               int temp = arr[j];
               arr[j] = arr[j+1];
               arr[j+1]=temp;
