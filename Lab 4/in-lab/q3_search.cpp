ae3a2fee058aad5c0b5ba6bbd1d0999819e1b34e
Aamna Rizwan <aamnarizwan7@gmail.com>
2025-09-12 12:44:31 +0500

    Update q3_search.cpp

--- Lab 4/in-lab/q3_search.cpp
+++ Lab 4/in-lab/q3_search.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int interpolationSearch(int arr[], int n, int x){
-    int low=0;
-    int high=n-1;
-    while(low<=high && x>=arr[low] && x<=arr[high]){
-        if(low=high){
-            if(arr[low]==x) return low;
-            return -1;
-        }
-        int pos = low +((x-arr[low])*(high-low))/arr[high]-arr[low];
-        if (arr[pos]==x) return pos;
-        if(arr[pos]<x) low=pos+1;
-        else high = pos-1;
+int interpolationSearch(int arr[], int lo, int hi, int x)
+{
+    int pos;
+    if (lo <= hi && x >= arr[lo] && x <= arr[hi]) {
+        pos = lo + ((double)(hi-lo) / (arr[hi]-arr[lo])*(x-arr[lo]));
+        if (arr[pos] == x)
+            return pos;
+        if (arr[pos] < x)
+            return interpolationSearch(arr, pos + 1, hi, x);
+        if (arr[pos] > x)
+            return interpolationSearch(arr, lo, pos - 1, x);
     }
     return -1;
 }
+
 
 int main() {
   int n;
@@ -47,9 +47,8 @@ int main() {
   int x;
   cout << "\nenter value to search\n";
   cin >> x;
-  int index = interpolationSearch(arr,n,x);
+  int index = interpolationSearch(arr,0,n-1,x);
   cout << "Value at index " << index << endl;
   delete[] arr;
-
  
 }
