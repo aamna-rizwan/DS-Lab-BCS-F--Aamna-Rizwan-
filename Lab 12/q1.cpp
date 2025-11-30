03b6fad39ee94f69682e4d17ce9190bc3224b87b
Aamna Rizwan <aamnarizwan7@gmail.com>
2025-11-30 19:29:54 +0500

    Update q1.cpp

--- Lab 12/q1.cpp
+++ Lab 12/q1.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+void bruteForce(string input, string pattern){
+    int n=input.length();
+    int m=pattern.length();
+    int c=0;
+    for(int i=0;i<=n-m;i++){
+        int j=0;
+        while(j<m && input[i+j]==pattern[j]){
+            j++;
+        }
+        if(j==m){
+            cout << "Pattern found at " << i << endl;
+        }
+        c++;
+    }
+    cout << "Number of comparisions taken : " << c << endl;
+}
+
+int main(){
+    string msg = "the quick brown fox jumps over the lazy dog";
+    string key = "the";
+    bruteForce(msg,key);
+}
