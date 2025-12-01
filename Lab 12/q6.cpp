00acb9c1d3b3cf7cb239eba8aec011841e5b7fb1
Aamna Rizwan <aamnarizwan7@gmail.com>
2025-12-02 02:33:46 +0500

    Implement Dijkstra's algorithm in q6.cpp
    Added Dijkstra's algorithm implementation as a comment.

--- Lab 12/q6.cpp
+++ Lab 12/q6.cpp
@@ -1,1 +1,23 @@
+//dijsktra algorithm written : 
+/*
 
+function Dijkstra(Graph, source, target):
+for each vertex v in Graph:
+define the visited state FALSE
+define the cost from the source node to infinity
+define the previous node to None
+if v == source:
+define the cost from the source node to zero
+while target is not visited:
+selected_node ← vertex in Graph with min distance from
+the source node
+selected_node is characterized as visited
+for each neighbor v of selected_node not visited:
+alt ← selected_node distance from source node +
+length(selected_node, v)
+if alt &lt; neighbor distance from source node:
+neighbor distance from source node ← alt
+neighbor previous node ← selected_node
+calculate the path
+return path, target node distance from start
+*/
